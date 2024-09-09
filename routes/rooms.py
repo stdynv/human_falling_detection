0abd13ce1796ca289af49325be412be1a97f396a
NabilHtr <nabilhatri8@gmail.com>
2024-09-09 20:44:26 +0200

    Ajout de l'interface d'accueil pour les routes API

--- routes/rooms.py
+++ routes/rooms.py
@@ -1,67 +1,62 @@
 from flask import Blueprint, request, jsonify
+from extensions import db
+from models import Room  
 
 rooms_bp = Blueprint('rooms_bp', __name__)
-
-rooms = []
-
-def find_item(items, item_id):
-    for item in items:
-        if item['room_id'] == item_id:
-            return item
-    return None
-
-
-def get_next_id(items):
-    if items:
-        return max(item['room_id'] for item in items) + 1
-    else:
-        return 1
-
+    
 @rooms_bp.route('/', methods=['POST'])
 def create_room():
     data = request.get_json()
-    room = {
-        'room_id': get_next_id(rooms),
-        'room_number': data.get('room_number'),
-        'floor': data.get('floor'),
-        'type': data.get('type'),
-        'occupied': data.get('occupied'),
-        'raspberry_id': data.get('raspberry_id')
-    }
-    rooms.append(room)
-    return jsonify(room), 201
+    new_room = Room(
+        room_number=data['room_number'],
+        floor=data['floor'],
+        type=data['type'],
+        occupied=data['occupied'],
+        raspberry_id=data['raspberry_id']
+    )
+    db.session.add(new_room)
+    db.session.commit()
+    return jsonify({'message': 'Room created'}), 201
 
 @rooms_bp.route('/', methods=['GET'])
 def get_rooms():
-    return jsonify(rooms), 200
+    rooms = Room.query.all()
+    return jsonify([{
+        'room_id': room.room_id,
+        'room_number': room.room_number,
+        'floor': room.floor,
+        'type': room.type,
+        'occupied': room.occupied,
+        'raspberry_id': room.raspberry_id
+    } for room in rooms]), 200
 
 @rooms_bp.route('/<int:room_id>', methods=['GET'])
 def get_room(room_id):
-    room = find_item(rooms, room_id)
-    if room:
-        return jsonify(room), 200
-    return jsonify({'error': 'Room not found'}), 404
-
+    room = Room.query.get_or_404(room_id)
+    return jsonify({
+        'room_id': room.room_id,
+        'room_number': room.room_number,
+        'floor': room.floor,
+        'type': room.type,
+        'occupied': room.occupied,
+        'raspberry_id': room.raspberry_id
+    }), 200
 
 @rooms_bp.route('/<int:room_id>', methods=['PUT'])
 def update_room(room_id):
-    room = find_item(rooms, room_id)
-    if room:
-        data = request.get_json()
-        room.update({
-            'room_number': data.get('room_number', room['room_number']),
-            'floor': data.get('floor', room['floor']),
-            'type': data.get('type', room['type']),
-            'occupied': data.get('occupied', room['occupied']),
-            'raspberry_id': data.get('raspberry_id', room['raspberry_id'])
-        })
-        return jsonify(room), 200
-    return jsonify({'error': 'Room not found'}), 404
+    room = Room.query.get_or_404(room_id)
+    data = request.get_json()
+    room.room_number = data.get('room_number', room.room_number)
+    room.floor = data.get('floor', room.floor)
+    room.type = data.get('type', room.type)
+    room.occupied = data.get('occupied', room.occupied)
+    room.raspberry_id = data.get('raspberry_id', room.raspberry_id)
+    db.session.commit()
+    return jsonify({'message': 'Room updated'}), 200
 
 @rooms_bp.route('/<int:room_id>', methods=['DELETE'])
 def delete_room(room_id):
-    room = find_item(rooms, room_id)
-    if room:
-        rooms.remove(room)
-        return jsonify({'message': 'Room deleted'}), 200
-    return jsonify({'error': 'Room not found'}), 404
+    room = Room.query.get_or_404(room_id)
+    db.session.delete(room)
+    db.session.commit()
+    return jsonify({'message': 'Room deleted'}), 200
