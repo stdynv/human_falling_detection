af93ff34c9e9de0e9bd2f64cbf3f2c7ac16c4a4f
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-11 21:56:46 +0200

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 # Import the routes from each module
 
-from .rooms import rooms_bp
+# from .rooms import rooms_bp
 from .incidents import incidents_bp
 from .staff import staff_bp
 
@@ -10,7 +10,7 @@ from .staff import staff_bp
 def create_routes(app):
     # Register the Blueprints with the Flask app
     
-    app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
+    # app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
     app.register_blueprint(incidents_bp, url_prefix='/api/incidents')
     app.register_blueprint(incidents_bp, url_prefix='/api/staff')
     
