c77d70b578ed116c988e4c4f035153cb2bd8f5e3
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-11 21:34:26 +0200

    inused api room in app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 
 
 # Import the Blueprints from the routes
-from routes.rooms import rooms_bp
+# from routes.rooms import rooms_bp
 from routes.incidents import incidents_bp
 from routes.staff import staff_bp
 from routes.azure_blob import azure_bp
 
 # Register the Blueprints with the Flask app
-app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
+# app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
 app.register_blueprint(incidents_bp, url_prefix='/api/incidents')
 app.register_blueprint(azure_bp, url_prefix='/api/azure')
 app.register_blueprint(staff_bp, url_prefix='/api/staff')
