ef45226b143a031a8cf8123acc0504d89416e105
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-11 21:15:50 +0200

    set the host in the app.py

--- app.py
+++ app.py
@@ -69,4 +69,4 @@ def test_db():
         return f"Error connecting to the database: {e}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True,port=8000,host="0.0.0.0")
