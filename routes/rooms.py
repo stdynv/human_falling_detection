e5811df18091499f1eda2e8f8272d76525bdb5bc
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-13 00:46:18 +0200

    Update rooms.py

--- routes/rooms.py
+++ routes/rooms.py
@@ -1,79 +1,10 @@
 from flask import Blueprint, request, jsonify
-from config import Config  
+from extensions import db
+from models import Room
 
 rooms_bp = Blueprint('rooms_bp', __name__)
     
-
-@rooms_bp.route('/', methods=['GET'])
-def get_rooms():
-    conn = Config.get_db_connection()
-    if conn is None:
-        return jsonify({"error": "Database connection failed"}), 500
-    
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Rooms")
-    
-    rooms = []
-    for row in cursor.fetchall():
-        rooms.append({
-            'room_id': row.room_id,
-            'room_number': row.room_number,
-            'floor': row.floor,
-            'occupied': row.occupied,
-            'raspberry_id' : row.raspberry_id
-        })
-
-    # emit('notifications',incidents)
-    
-    conn.close()
-    return jsonify(rooms)
-
 @rooms_bp.route('/create', methods=['POST'])
-def create_room():
-    try:
-        # Obtenir la connexion à la base de données
-        conn = Config.get_db_connection()  # Remplacer par ta méthode de connexion à la DB
-        if conn is None:
-            # logging.error('Database connection failed.')
-            return jsonify({'error': 'Database connection failed'}), 500
-        
-        # Récupérer les données de la requête
-        data = request.get_json()
-        
-        # Définir la requête SQL
-        sql = """INSERT INTO rooms (room_number, floor, type, occupied, raspberry_id)
-                 VALUES (?, ?, ?, ?, ?)"""
-        
-        # Définir les valeurs de la requête
-        values = (
-            data['room_number'], 
-            data['floor'], 
-            data['type'], 
-            0, 
-            data['raspberry_id']
-        )
-        
-        # Log la requête SQL pour le débogage
-        logging.info(f"Executing SQL: {sql} with values {values}")
-        
-        # Exécuter la requête SQL
-        cursor = conn.cursor()
-        cursor.execute(sql, values)
-        conn.commit()
-        
-        # Fermer le curseur et la connexion
-        cursor.close()
-        conn.close()
-
-        # Retourner un message de succès
-        return jsonify({'message': 'Room created successfully'}), 201
-
-    except Exception as e:
-        # Log en cas d'erreur
-        logging.error(f"Error occurred: {e}")
-        return jsonify({'error': 'An error occurred'}), 500
-
-"""@rooms_bp.route('/create', methods=['POST'])
 def create_room():
     data = request.get_json()
     new_room = Room(
@@ -128,4 +59,4 @@ def delete_room(room_id):
     room = Room.query.get_or_404(room_id)
     db.session.delete(room)
     db.session.commit()
-    return jsonify({'message': 'Room deleted'}), 200"""
+    return jsonify({'message': 'Room deleted'}), 200
