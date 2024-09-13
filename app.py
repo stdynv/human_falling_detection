988254f200ccf6f1d641831aa5fd42b985e2704b
Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-13 11:26:02 +0200

    clean routes app.py

--- app.py
+++ app.py
@@ -30,45 +30,6 @@ app.register_blueprint(incidents_bp, url_prefix='/api/incidents')
 app.register_blueprint(azure_bp, url_prefix='/api/azure')
 app.register_blueprint(staff_bp, url_prefix='/api/staff')
 
-@app.route('/documentation')
-def home():
-    html = """
-    <html>
-        <head>
-            <title>Human Falling Detection API</title>
-            <style>
-                body { font-family: Arial, sans-serif; margin: 40px; }
-                h1 { color: #333; }
-                a { text-decoration: none; color: #007BFF; }
-                ul { list-style-type: none; padding: 0; }
-                li { margin: 10px 0; }
-                .container { width: 80%; margin: auto; }
-            </style>
-        </head>
-        <body>
-            <div class="container">
-                <h1>Bienvenue sur l'interface de l'API</h1>
-                <p>Cliquez sur les liens ci-dessous pour accéder aux différentes routes de l'API :</p>
-                <ul>
-                    <li><a href="/api/rooms">Gestion des chambres (Rooms)</a></li>
-                    <li><a href="/api/incidents">Gestion des incidents (Incidents)</a></li>
-                    <li><a href="/api/azure">Stockage Azure (Azure Blob)</a></li>
-                    <li><a href="/api/staff">Gestion du personnel (Staff)</a></li>
-                    <li><a href="/test_db">Tester la connexion à la base de données</a></li>
-                </ul>
-            </div>
-        </body>
-    </html>
-    """
-    return render_template_string(html)
-
-@app.route('/test_db')
-def test_db():
-    try:
-        db.session.execute(text('SELECT 1'))
-        return "Database connection is working!", 200
-    except Exception as e:
-        return f"Error connecting to the database: {e}", 500
 
 @app.route('/')
 def test_socket():
