009d9d160c658a45d736c76b2c1ee8b97b564816
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-15 21:39:49 +0200

    fix socketio server

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
+import eventlet
+eventlet.monkey_patch()
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from extensions import db, socketio  # Import socketio from extensions
+from extensions import db, socketio
 
 app = Flask(__name__)
 
 # Initialize extensions
 db.init_app(app)
-socketio.init_app(app, cors_allowed_origins=["https://flask-ehpad-fde5f2fndkd0f2gk.eastus-01.azurewebsites.net",'*'])
+socketio.init_app(app, cors_allowed_origins="*", async_mode="eventlet")
 CORS(app, supports_credentials=True)
 
 # Import the Blueprints from the routes
 from routes.rooms import rooms_bp
 from routes.incidents import incidents_bp
 from routes.staff import staff_bp
-# from routes.azure_blob import azure_bp
 
 # Register the Blueprints with the Flask app
 app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
@@ -25,6 +26,5 @@ app.register_blueprint(incidents_bp, url_prefix='/api/incidents')
 def home():
     return render_template('main.html')
 
-# Run the app using SocketIO instead of app.run
 if __name__ == '__main__':
-    socketio.run(app, debug=True, port=8000, host="0.0.0.0")
+    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
