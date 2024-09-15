98697e8f44e480888b7ea7b23c5b2d957920c783
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-15 21:45:55 +0200

    update app.py

--- app.py
+++ app.py
@@ -1,30 +1,38 @@
-import eventlet
-eventlet.monkey_patch()
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from extensions import db, socketio
+from extensions import db, socketio  # Import socketio from extensions
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    'mssql+pyodbc://ehpad-admin:Memoire2024!@ehpadserver.database.windows.net:1433/ehpad?driver=ODBC+Driver+18+for+SQL+Server'
+)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # Initialize extensions
 db.init_app(app)
-socketio.init_app(app, cors_allowed_origins="*", async_mode="eventlet")
+socketio.init_app(app, cors_allowed_origins=['*'])
 CORS(app, supports_credentials=True)
 
 # Import the Blueprints from the routes
 from routes.rooms import rooms_bp
 from routes.incidents import incidents_bp
 from routes.staff import staff_bp
+# from routes.azure_blob import azure_bp
 
 # Register the Blueprints with the Flask app
 app.register_blueprint(rooms_bp, url_prefix='/api/rooms')
 app.register_blueprint(incidents_bp, url_prefix='/api/incidents')
+# app.register_blueprint(azure_bp, url_prefix='/api/azure')
+# app.register_blueprint(staff_bp, url_prefix='/api/staff')
 
 @app.route('/')
-def home():
+def test_socket():
     return render_template('main.html')
 
+# Run the app using SocketIO instead of app.run
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host="0.0.0.0", port=8000)
+    socketio.run(app, debug=True, port=8000, host="0.0.0.0")
