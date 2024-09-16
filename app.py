533c85cf06fdb29cbbf32bd1a35b50a1a2925640
Mohamed Yassine Essamadi <78117993+stdynv@users.noreply.github.com>
2024-09-16 22:31:11 +0200

    remove eventlet

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-import eventlet
-# Monkey patching for eventlet
-eventlet.monkey_patch()
-
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
